5f63bc03d8b1147d11598bb9796b24c4951f452d
Denis <denis.boghiu@gmail.com>
2022-09-25 19:48:43 +0300

    using graphql_object to be able to define additional computed fields

--- src/searchapp/model.rs
+++ src/searchapp/model.rs
@@ -1,15 +1,42 @@
-use juniper::GraphQLObject;
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, Serialize, Deserialize, GraphQLObject)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Post {
     pub id: String,
-    pub address: Option<String>,
     pub category: String,
+    pub address: Option<String>,
     pub subcategory: Option<String>,
     pub created: Option<String>,
     pub description: Option<String>,
     pub title: Option<String>,
+}
+
+#[juniper::graphql_object]
+impl Post {
+    async fn id(&self) -> String {
+        self.id.clone()
+    }
+    async fn category(&self) -> String {
+        self.category.clone()
+    }
+    async fn address(&self) -> Option<String> {
+        self.address.clone()
+    }
+    async fn subcategory(&self) -> Option<String> {
+        self.subcategory.clone()
+    }
+    async fn created(&self) -> Option<String> {
+        self.created.clone()
+    }
+    async fn description(&self) -> Option<String> {
+        self.description.clone()
+    }
+    async fn title(&self) -> Option<String> {
+        self.title.clone()
+    }
+    async fn photos(&self) -> String {
+        "some photo".to_string()
+    }
 }
 
 impl Post {
